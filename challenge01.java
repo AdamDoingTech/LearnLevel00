0e1ae7688d6b9b4d573e92285b1b13ca2da7b5f5
Adam <olmpy@hotmail.com>
2025-02-26 01:04:48 -0700

    Adding intial files

---
+++ challenge01.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class challenge01 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		// user entering their name.
+		System.out.print("Please enter your name: ");
+		String nameInput = input.nextLine();
+
+		// user entering their age.
+		System.out.print("Please enter your age: ");
+		int ageInput = input.nextInt();
+
+			 	while (ageInput < 0) {
+
+			 		System.out.print("Wrong input, please enter a correct number: ");
+			 		ageInput = input.nextInt();
+
+			 	} if (ageInput >= 18) {
+			 		
+			 		System.out.println("You are above the age limit, and you are allowed to vote, please go to your voting booth.");
+			 	
+			 	} else if (ageInput <= 17 && ageInput >= 0) {
+			 	
+			 	System.out.println("You are under age, and you are not allowed to vote just yet.");
+			}
+
+		// thank you message 
+		System.out.println("Thank you for using the software, I hope that you have a good day.");
+		// closing scanner
+		input.close(); 
+		}
+	} 
+
